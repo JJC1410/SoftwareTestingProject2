99132b8ae7f62f38656bc9046f9a28ad13aea6d9
JJC1410 <qianzheng.jiang@gmail.com>
2021-04-09 01:13:06 -0400

    Step 4

--- src/Movie.java
+++ src/Movie.java
@@ -28,4 +28,36 @@ public class Movie
     {
     	return title;
     }
+    
+    public double charge(int daysRented)
+    {
+    	double charge = 0;
+    	
+    	switch (this.getPriceCode()) 
+	    {
+	    	case Movie.REGULAR:
+	    	charge += 2;
+	    	if (daysRented > 2)
+	    	{
+	    		charge += (daysRented - 2) * 1.5;
+
+	    	}
+	    	break;
+	    	
+	    	case Movie.NEW_RELEASE:
+	    	charge += daysRented * 3;
+	    	break;
+	    	
+	    	case Movie.CHILDRENS:
+		
+	    	charge += 1.5;
+	    	if (daysRented > 3)
+	    	{
+	    		charge += (daysRented - 3) * 1.5;
+	    	}
+	    	break;
+	    }
+    	
+    	return charge;
+    }
 }
--- src/Rental.java
+++ src/Rental.java
@@ -22,34 +22,7 @@ public class Rental
 
     public double charge()
     {
-		double charge = 0;
-	    //determine amounts a particular rental
-		
-	    switch (this.getMovie().getPriceCode()) 
-	    {
-	    	case Movie.REGULAR:
-	    	charge += 2;
-	    	if (this.getDaysRented() > 2)
-	    	{
-	    		charge += (this.getDaysRented() - 2) * 1.5;
-
-	    	}
-	    	break;
-	    	
-	    	case Movie.NEW_RELEASE:
-	    	charge += this.getDaysRented() * 3;
-	    	break;
-	    	
-	    	case Movie.CHILDRENS:
-		
-	    	charge += 1.5;
-	    	if (this.getDaysRented() > 3)
-	    	{
-	    		charge += (this.getDaysRented() - 3) * 1.5;
-	    	}
-	    	break;
-	    }
-		return charge;
+    	return movie.charge(daysRented);
     }
 }
 
